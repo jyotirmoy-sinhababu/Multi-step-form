90547c853c28c9a2e10220c428e6a9a0abcae183
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-03-15 00:48:41 +0530

    your info comp created

---
+++ src/components/info/YourInfo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const YourInfo = () => {
+  return (
+    <div>
+      <input type='txt' name='name' placeholder='e.g. Shiva' />
+      <input type='txt' name='email' placeholder='e.g. Shiva@lorem.com' />
+      <input type='number' name='number' placeholder='e.g. 91 74512875' />
+    </div>
+  );
+};
+
+export default YourInfo;
---
+++ src/pages/home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import YourInfo from '../../components/info/YourInfo';
+
+const Home = () => {
+  return (
+    <div>
+      <form>
+        <YourInfo />
+      </form>
+    </div>
+  );
+};
+
+export default Home;
