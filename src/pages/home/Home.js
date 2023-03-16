e21d2661f91b77b1966f8fac9d09e45e5c2832f0
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-03-16 23:33:49 +0530

    next btn, back btn and confirm btn created

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -4,6 +4,7 @@ import SideBar from '../../components/sideBar/SideBar';
 import Main from '../main/Main';
 
 const Home = () => {
+  // All states
   const [inputData, setInputData] = useState({
     email: '',
     name: '',
@@ -14,10 +15,25 @@ const Home = () => {
     addOnsYearly: '',
   });
 
+  const [isOn, setIsOn] = useState(true);
+  const [count, setCount] = useState(1);
+
+  // control form navigation
+
+  const nextBtn = () => {
+    if (count < 4) setCount((prev) => prev + 1);
+  };
+
+  const backBtn = () => {
+    if (count > 1) {
+      setCount((prev) => prev - 1);
+    }
+  };
+
   return (
     <div>
-      <SideBar />
-      <Main />
+      <SideBar count={count} />
+      <Main nextBtn={nextBtn} backBtn={backBtn} count={count} />
     </div>
   );
 };
--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -4,14 +4,27 @@ import YourInfo from '../../components/info/YourInfo';
 import SelectPlan from '../../components/plan/SelectPlan';
 import AddOn from '../../components/addOns/AddOn';
 
-const Main = () => {
+const Main = ({ count, backBtn, nextBtn }) => {
+  console.log(count);
   return (
     <div>
       <form>
-        <YourInfo />
-        <SelectPlan />
-        <AddOn />
+        {count == 1 ? (
+          <YourInfo />
+        ) : count == 2 ? (
+          <SelectPlan />
+        ) : count == 3 ? (
+          <AddOn />
+        ) : null}
       </form>
+      <div>
+        {count > 1 ? <button onClick={backBtn}>Go Back</button> : null}
+        {count == 4 ? (
+          <button>confirm</button>
+        ) : (
+          <button onClick={nextBtn}>Next</button>
+        )}
+      </div>
     </div>
   );
 };
