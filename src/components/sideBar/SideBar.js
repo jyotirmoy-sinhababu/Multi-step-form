eb840854c0dd479b50aa096d0d046ebc31e44b15
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-03-16 17:14:22 +0530

    sidebar created

---
+++ src/components/sideBar/SideBar.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+const SideBar = () => {
+  return (
+    <>
+      <div>
+        <div>
+          <p>1</p>
+        </div>
+        <div>
+          <p>STEP 1</p>
+          <p>YOUR INFO</p>
+        </div>
+      </div>
+      <div>
+        <div>
+          <p>2</p>
+        </div>
+        <div>
+          <p>STEP 2</p>
+          <p>SELECT YOUR PLAN</p>
+        </div>
+      </div>
+      <div>
+        <div>
+          <p>3</p>
+        </div>
+        <div>
+          <p>STEP 3</p>
+          <p>ADD-ONS</p>
+        </div>
+      </div>
+      <div>
+        <div>
+          <p>4</p>
+        </div>
+        <div>
+          <p>STEP 4</p>
+          <p>SUMMARY</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SideBar;
