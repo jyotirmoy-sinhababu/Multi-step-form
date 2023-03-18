5a4d794a3dd2881a469019ebfcce3c00007b8356
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-03-18 12:43:59 +0530

    style added to yourInfo cnt and some changes made

--- src/components/info/YourInfo.js
+++ src/components/info/YourInfo.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import './info.css';
+
 const YourInfo = ({ setInputData, inputData }) => {
   // monitor changes
   const handleChange = (e) => {
@@ -9,30 +11,48 @@ const YourInfo = ({ setInputData, inputData }) => {
 
   return (
     <div>
-      <input
-        onChange={(e) => {
-          handleChange(e);
-        }}
-        type='txt'
-        name='name'
-        placeholder='e.g. Shiva'
-      />
-      <input
-        onChange={(e) => {
-          handleChange(e);
-        }}
-        type='txt'
-        name='email'
-        placeholder='e.g. Shiva@lorem.com'
-      />
-      <input
-        onChange={(e) => {
-          handleChange(e);
-        }}
-        type='number'
-        name='number'
-        placeholder='e.g. 91 74512875'
-      />
+      <h2>Personal info</h2>
+      <p>Please provide your name,email address and phone number</p>
+      <div className='info-input-cnt'>
+        <div className='input-cnt'>
+          {' '}
+          <label className='input-label'>Name</label>
+          <input
+            onChange={(e) => {
+              handleChange(e);
+            }}
+            className='input-box'
+            type='txt'
+            name='name'
+            placeholder='e.g. Shiva'
+          />
+        </div>
+        <div className='input-cnt'>
+          <label className='input-label'>Email Address</label>
+          <input
+            onChange={(e) => {
+              handleChange(e);
+            }}
+            className='input-box'
+            type='txt'
+            name='email'
+            placeholder='e.g. Shiva@lorem.com'
+          />
+        </div>
+        <div className='input-cnt'>
+          {' '}
+          <label className='input-label'>Phone Number</label>
+          <input
+            onChange={(e) => {
+              handleChange(e);
+            }}
+            className='input-box'
+            type='number'
+            name='number'
+            placeholder='e.g. 91 74512875'
+          />
+        </div>
+      </div>
     </div>
   );
 };
