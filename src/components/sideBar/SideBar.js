4f3e4d4905ed5df43bb6b38d2eed3b8a7c727bbc
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-03-20 22:39:33 +0530

    style added to sidebar comp

--- src/components/sideBar/SideBar.js
+++ src/components/sideBar/SideBar.js
@@ -4,12 +4,14 @@ import React from 'react';
 
 import './sidebar.css';
 
-const SideBar = () => {
+const SideBar = ({ count }) => {
   return (
     <div className='sidebar-cnt'>
       {/* <Banner /> */}
       <div className='sidebar-small-cnt'>
-        <div className='sidebar-num-cnt'>
+        <div
+          className={`sidebar-num-cnt ${count == 1 ? 'active' : 'inactive'}`}
+        >
           <p>1</p>
         </div>
         <div>
@@ -18,7 +20,9 @@ const SideBar = () => {
         </div>
       </div>
       <div className='sidebar-small-cnt'>
-        <div className='sidebar-num-cnt'>
+        <div
+          className={`sidebar-num-cnt ${count == 2 ? 'active' : 'inactive'}`}
+        >
           <p>2</p>
         </div>
         <div>
@@ -27,7 +31,9 @@ const SideBar = () => {
         </div>
       </div>
       <div className='sidebar-small-cnt'>
-        <div className='sidebar-num-cnt'>
+        <div
+          className={`sidebar-num-cnt ${count == 3 ? 'active' : 'inactive'}`}
+        >
           <p>3</p>
         </div>
         <div>
@@ -36,7 +42,9 @@ const SideBar = () => {
         </div>
       </div>
       <div className='sidebar-small-cnt'>
-        <div className='sidebar-num-cnt'>
+        <div
+          className={`sidebar-num-cnt ${count == 4 ? 'active' : 'inactive'}`}
+        >
           <p>4</p>
         </div>
         <div>
