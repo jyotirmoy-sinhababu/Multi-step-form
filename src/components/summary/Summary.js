c900257eced399ba89bcc9ccde64bb3890abbb2a
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-03-24 00:19:40 +0530

    style added to summary-comp

--- src/components/summary/Summary.js
+++ src/components/summary/Summary.js
@@ -1,38 +1,55 @@
 import React from 'react';
 
+import './summary.css';
+
 const Summary = ({ inputData, isActive }) => {
   console.log(inputData.monthlyPlan);
   return (
-    <div>
-      <div>
-        <h2>Finishing up</h2>
-        <p>Double-check everything looks OK before confirming</p>
+    <div className='summary-main-div'>
+      <div className='summary-heading-cnt'>
+        <h2 className='summary-header'>Finishing up</h2>
+        <p className='summary-para'>
+          Double-check everything looks OK before confirming
+        </p>
       </div>
       {isActive ? (
-        <div>
-          <div>
-            <div>
-              <div>
-                <p>Arcade</p>
+        <div className=''>
+          <div className='summary-data-cnt'>
+            <div className='summary-arcade-cnt'>
+              <div className='summary-arcade-txt-cnt'>
+                <p className='arcade-txt'>Arcade</p>
 
-                <p>(monthly)</p>
+                <p className='arcade-monthly'>(monthly)</p>
               </div>
-              <div>{inputData ? <p>{inputData.monthlyPlan}</p> : null}</div>
+              <div className='arcade-data-cnt'>
+                {inputData ? (
+                  <p className='arcade-data'>{inputData.monthlyPlan}</p>
+                ) : null}
+              </div>
             </div>
-            <div>
-              <p>Online service</p>
-              {inputData ? <p>{inputData.monthlyService}</p> : null}
-            </div>
-            <div>
-              <p>Larger Storage</p>
-              {inputData ? <p>{inputData.monthlyStorage}</p> : null}
+            <div className='storage-service-cnt'>
+              {' '}
+              <div className='online-service-cnt'>
+                <p className='online-service-txt'>Online service</p>
+                {inputData ? (
+                  <p className='online-service-data'>
+                    {inputData.monthlyService}
+                  </p>
+                ) : null}
+              </div>
+              <div className='storage-cnt'>
+                <p className='storage-txt'>Larger Storage</p>
+                {inputData ? (
+                  <p className='storage-data'> {inputData.monthlyStorage}</p>
+                ) : null}
+              </div>
             </div>
           </div>
-          <div>
-            <div>
-              <p>Total </p>
+          <div className='summary-total-cnt'>
+            <div className='summary-total-data-cnt'>
+              <p className='total-txt'>Total </p>
               {inputData ? (
-                <p>
+                <p className='total-data'>
                   $
                   {+inputData.monthlyPlan +
                     +inputData.monthlyService +
@@ -41,9 +58,6 @@ const Summary = ({ inputData, isActive }) => {
               ) : (
                 <p>0</p>
               )}
-            </div>
-            <div>
-              <p></p>
             </div>
           </div>
         </div>
